506652489e25ecaa181debe77f7473cfe13cb13c
Michael Benjamin Backus <mbbbackus@gmail.com>
2024-05-18 09:11:52 -0400

    Add update endpoint

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("list/", views.post_list, name="post_list"),
+    path('edit/<int:pk>/', views.update_post, name='update_post'),
 ]
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .models import Post
+import json
 
 def post_list(request):
     posts = Post.objects.all()
@@ -16,4 +17,20 @@ def post_list(request):
 
 def index(request):
     return HttpResponse("Hello, world. You're at the posts index.")
+
+
+@csrf_exempt
+def update_post(request, pk):
+    data = json.loads(request.body)
+    if request.method == 'PUT':
+        try:
+            post = Post.objects.get(pk=pk)
+            post.title = data['title']
+            post.content = data['content']
+            post.save()
+            return JsonResponse({'status': 'success', 'message': 'post updated successfully'})
+        except Post.DoesNotExist:
+            return JsonResponse({'status': 'error', 'message': 'post not found'}, status=404)
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=405)
 
